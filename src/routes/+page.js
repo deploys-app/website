8e03df15dd03382a8ebdbcc19a058fa2a5ef96b2
Thanatat Tamtan <acoshift@gmail.com>
2022-09-06 21:37:43 +0700

    update dependencies

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,10 @@
 export async function load ({ fetch }) {
-	const resp = await fetch('https://api.deploys.app/billing.skus')
+	const resp = await fetch('https://api.deploys.app/billing.skus', {
+		// workaround for ssr fetch not sending origin but verify cors response headers
+		headers: {
+			origin: 'localhost'
+		}
+	})
 	const res = await resp.json()
 	return {
 		price: res.result
