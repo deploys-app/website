73da4c2122885f90c6acce532895b76ff5f1ecbd
Thanatat Tamtan <acoshift@gmail.com>
2022-11-29 12:17:20 +0700

    try new workaround

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,7 @@
 export async function load ({ fetch }) {
-	const resp = await fetch('https://api.deploys.app/billing.skus')
+	const resp = await fetch('https://api.deploys.app/billing.skus', {
+		credentials: 'omit'
+	})
 	const res = await resp.json()
 	return {
 		price: res.result
