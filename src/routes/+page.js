e1049afef85c1a5acbdccc1e2220e69b65853ae5
Thanatat Tamtan <acoshift@gmail.com>
2022-11-29 11:49:02 +0700

    remove workaround

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,5 @@
 export async function load ({ fetch }) {
-	const resp = await fetch('https://api.deploys.app/billing.skus', {
-		// workaround for ssr fetch not sending origin but verify cors response headers
-		headers: {
-			origin: 'localhost'
-		}
-	})
+	const resp = await fetch('https://api.deploys.app/billing.skus')
 	const res = await resp.json()
 	return {
 		price: res.result
